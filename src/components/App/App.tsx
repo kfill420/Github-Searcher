d6af0ef950a97ec87a6f6f39db8b317d1d20c19c
kfill420 <alexisvignot@hotmail.fr>
2024-05-26 00:31:35 +0200

    fix: routes

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,38 +1,19 @@
-import { Image, Container, Segment, Menu, MenuItem } from 'semantic-ui-react';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import { Link, NavLink, Route, Routes } from 'react-router-dom';
+import { Image, Menu, MenuItem } from 'semantic-ui-react';
+import { NavLink, Route, Routes } from 'react-router-dom';
 
-import logo from '../../assets/logo.svg';
 import githubLogo from '../../assets/images/logo-github.png';
 
 import './App.scss';
-import SearchBar from '../SearchBar/SearchBar';
-import Message from '../Message/Message';
-import ReposResults from '../ReposResults/ReposResults';
-import IListRepos from '../../@types/IListRepos.d';
-import MoreResultButton from '../MoreResultButton/MoreResultButton';
-import Loader from '../Loader/Loader';
 import HomePage from '../HomePage/HomePage';
 import FaqPage from '../FaqPage/FaqPage';
 import Error404 from '../Error404/Error404';
-
-interface DataListRepos {
-  id: number;
-  name: string;
-  owner: {
-    login: string;
-    avatar_url: string;
-  };
-  description: string;
-}
 
 function App() {
   return (
     <div className="App">
       <Image src={githubLogo} size="small" centered />
       <Menu>
-        <MenuItem as={NavLink} to="/">
+        <MenuItem as={NavLink} to="/Github-Searcher">
           Recherche
         </MenuItem>
         <MenuItem as={NavLink} to="/faq">
@@ -40,7 +21,7 @@ function App() {
         </MenuItem>
       </Menu>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/Github-Searcher" element={<HomePage />} />
         <Route path="/faq" element={<FaqPage />} />
         <Route path="*" element={<Error404 />} />
       </Routes>
